c8171742c65bd2e69892c703d20e6607f2855c1f
Nilesh Fatfatwale <89182735+nilesh-fatfatwale@users.noreply.github.com>
2024-11-30 21:35:14 +0530

    Update CartProduct.js

--- src/components/menu/CartProduct.js
+++ src/components/menu/CartProduct.js
@@ -1,17 +1,15 @@
-import {cartProductPrice} from "@/components/AppContext";
+import { cartProductPrice } from "@/components/AppContext";
 import Trash from "@/components/icons/Trash";
 import Image from "next/image";
 
-export default function CartProduct({product,onRemove}) {
+export default function CartProduct({ product, index, onRemove }) {
   return (
     <div className="flex items-center gap-4 border-b py-4">
       <div className="w-24">
         <Image width={240} height={240} src={product.image} alt={''} />
       </div>
       <div className="grow">
-        <h3 className="font-semibold">
-          {product.name}
-        </h3>
+        <h3 className="font-semibold">{product.name}</h3>
         {product.size && (
           <div className="text-sm">
             Size: <span>{product.size.name}</span>
@@ -32,8 +30,9 @@ export default function CartProduct({product,onRemove}) {
         <div className="ml-2">
           <button
             type="button"
-            onClick={() => onRemove(index)}
-            className="p-2">
+            onClick={() => onRemove(index)}  
+            className="p-2"
+          >
             <Trash />
           </button>
         </div>
